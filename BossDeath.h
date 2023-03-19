f2928c81fad26e85f50755e59a774d33e16d51b9
PhamSon2k4 <127081003+PhamSon2k4@users.noreply.github.com>
2023-03-19 22:14:45 +0700

    first commit

---
+++ BossDeath.h
@@ -0,0 +1,23 @@
+#ifndef BOSSDEATH_H_
+#define BOSSDEATH_H_
+#include "BaseOject.h"
+
+class BossDeath : public BaseObject
+{
+public:
+    BossDeath();
+
+    bool GetIsShown() { return is_shown_; };
+    void SetIsShown(bool x) { is_shown_ = x; };
+
+    void ShowAnimation(SDL_Renderer* ren);
+    void HandleAnimation();
+    bool ShowEnough();
+private:
+    bool is_shown_ = 0;
+    SDL_Rect rectTexture[7];
+    int frame_count_ = 0;
+};
+
+#endif // BOSSDEATH_H_
+#pragma once
